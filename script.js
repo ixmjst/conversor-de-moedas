75c66dfd5063fceffa66e346c38cb7433a2505ac
ixmjst <tjosesimao@gmail.com>
2026-09-23 03:36:51 +0100

    feat:Capturando o evento de submit do formulário

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// Obtendo os elementos do formulário.
+const form= document.querySelector("form")
 const amount=document.querySelector("#amount")
+const currency=document.querySelector("#currency")
 
 // Manipulando o input amount para receber somente números
 amount.addEventListener("input",()=>{
    console.log(amount.value)
    const hasCharacterRegex=/\D+/g
     amount.value=amount.value.replace(hasCharacterRegex,"")
-    
+
 })
+
+//Capturando o evento de submit do formulário
+form.onsubmit=(event)=>{
+ event.preventDefault()
+ console.log(currency.value)
+}
